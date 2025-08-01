d9f9ba25418802453932438c58fbaa1c51d8962c
thanapat <thanapatspongpipat@gmail.com>
2025-08-02 00:13:41 +0700

    update: api weather

--- next.config.ts
+++ next.config.ts
@@ -2,29 +2,29 @@ import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  redirects: async () => {
-    return [
-      {
-        source: '/',
-        destination: '/weather',
-        permanent: true,
-      },
-    ];
-  },
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  env: {
-    API_KEY: process.env.API_KEY,
-    API_URL: process.env.API_URL,
-  },
-  /* config options here */
-  images: {
-    localPatterns: [
-      {
-        pathname: 'assets/images/**',
-        search: ''
-      }
-    ]
-  }
+	redirects: async () => {
+		return [
+			{
+				source: "/",
+				destination: "/weather",
+				permanent: true,
+			},
+		];
+	},
+	pageExtensions: ["ts", "tsx", "js", "jsx"],
+	env: {
+		API_KEY: process.env.API_KEY,
+		API_URL: process.env.API_URL,
+	},
+	/* config options here */
+	images: {
+		localPatterns: [
+			{
+				pathname: "assets/images/**",
+				search: "",
+			},
+		],
+	},
 };
 
 export default nextConfig;
--- src/app/(pages)/weather/page.tsx
+++ src/app/(pages)/weather/page.tsx
@@ -8,50 +8,50 @@ import { useAtomValue } from "jotai";
 import { countryStateAtom } from "@/atoms/country";
 
 const Weather = () => {
-  const [location, setLocation] = useState<LatLng>(
-    new LatLng(13.7563, 100.5018)
-  );
-  const { data: weatherData } = useGetWeatherByLatLong(location);
-  const country = useAtomValue(countryStateAtom);
+	const [location, setLocation] = useState<LatLng>(
+		new LatLng(13.7563, 100.5018)
+	);
+	const { data: weatherData } = useGetWeatherByLatLong(location);
+	const country = useAtomValue(countryStateAtom);
 
-  const onMapSelect = (latlng: LatLng) => {
-    setLocation(latlng);
-  };
+	const onMapSelect = (latlng: LatLng) => {
+		setLocation(latlng);
+	};
 
-  useEffect(() => {
-    if (country) {
-      setLocation(new LatLng(country.lat, country.lng));
-    }
-  }, [country]);
+	useEffect(() => {
+		if (country) {
+			setLocation(new LatLng(country.lat, country.lng));
+		}
+	}, [country]);
 
-  const iconUrl = `http://openweathermap.org/img/wn/${weatherData?.weather[0].icon}@4x.png`;
-  return (
-    <div className="flex flex-col w-full h-full md:flex-row">
-      <div className="flex w-full h-1/2 relative md:w-1/2 md:h-full">
-        <Map onSelect={onMapSelect} position={location} />
-      </div>
-      <div className="flex flex-col relative w-full h-1/2 items-center justify-center md:w-1/2 md:h-full">
-        <CountrySelector className="absolute top-6" />
-        <div className="flex flex-col items-center justify-center">
-          <img
-            src={iconUrl}
-            alt={weatherData?.weather[0].description}
-            className="object-cover aspect-square md:w-96  w-60 h-60"
-          />
-        </div>
-        <div className="flex flex-col items-center justify-center gap-1">
-          <span className="text-2xl font-bold">{weatherData?.name}</span>
-          <span>{weatherData?.sys.country}</span>
-          <span>
-            {weatherData?.main.temp
-              ? (weatherData?.main.temp - 273.15).toFixed(2)
-              : 0}
-            °C
-          </span>
-        </div>
-      </div>
-    </div>
-  );
+	const iconUrl = `http://openweathermap.org/img/wn/${weatherData?.weather[0].icon}@4x.png`;
+	return (
+		<div className="flex flex-col w-full h-full md:flex-row">
+			<div className="flex w-full h-1/2 relative md:w-1/2 md:h-full">
+				<Map onSelect={onMapSelect} position={location} />
+			</div>
+			<div className="flex flex-col relative w-full h-1/2 items-center justify-center md:w-1/2 md:h-full">
+				<CountrySelector className="absolute top-6" />
+				<div className="flex flex-col items-center justify-center">
+					<img
+						src={iconUrl}
+						alt={weatherData?.weather[0].description}
+						className="object-cover aspect-square md:w-96  w-60 h-60"
+					/>
+				</div>
+				<div className="flex flex-col items-center justify-center gap-1">
+					<span className="text-2xl font-bold">{weatherData?.name}</span>
+					<span>{weatherData?.sys.country}</span>
+					<span>
+						{weatherData?.main.temp
+							? (weatherData?.main.temp - 273.15).toFixed(2)
+							: 0}
+						°C
+					</span>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Weather;
